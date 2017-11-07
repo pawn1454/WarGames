4facf12de47e509b01d2a43b5d5e9ab92c0e1b03
pawn1454 <pawn1454@gmail.com>
2017-11-07 11:38:34 +0900

    Create cobolt.c

---
+++ Hackerschool-LoB-Redhat/gremlin/cobolt.c
@@ -0,0 +1,16 @@
+/*
+        The Lord of the BOF : The Fellowship of the BOF
+        - cobolt
+        - small buffer
+*/
+
+int main(int argc, char *argv[])
+{
+    char buffer[16];
+    if(argc < 2){
+        printf("argv error\n");
+        exit(0);
+    }
+    strcpy(buffer, argv[1]);
+    printf("%s\n", buffer);
+}
