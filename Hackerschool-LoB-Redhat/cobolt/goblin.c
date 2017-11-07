c5d5a4190d72141a2fc224efaf52bc7f4186e6f9
pawn1454 <pawn1454@gmail.com>
2017-11-07 12:05:49 +0900

    Create goblin.c

---
+++ Hackerschool-LoB-Redhat/cobolt/goblin.c
@@ -0,0 +1,12 @@
+/*
+        The Lord of the BOF : The Fellowship of the BOF
+        - goblin
+        - small buffer + stdin
+*/
+
+int main()
+{
+    char buffer[16];
+    gets(buffer);
+    printf("%s\n", buffer);
+}
